788432bbc63d53a1cd027ca5dccb9379a74fcbd8
tamu-b <tamurahideaki.b@gmail.com>
2026-07-09 00:56:33 +0900

    prettierのinstall

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,9 +1,11 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -11,12 +13,14 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
-  ...storybook.configs["flat/recommended"]
+  ...storybook.configs['flat/recommended'],
+  eslintPluginPrettierRecommended,
+  eslintConfigPrettier,
 ]);
 
 export default eslintConfig;
