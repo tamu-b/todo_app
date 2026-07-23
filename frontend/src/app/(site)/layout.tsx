12fba3d7e1ad2034c33d53a249c50f9624429fd3
tamu-b <tamurahideaki.b@gmail.com>
2026-07-23 19:51:25 +0900

    layoutの仮作成

---
+++ frontend/src/app/(site)/layout.tsx
@@ -0,0 +1,34 @@
+'use client';
+import { AppShell, Burger, Group } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+      padding="md"
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { mobile: !opened },
+      }}
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <div>Logo</div>
+        </Group>
+      </AppShell.Header>
+
+      <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+
+      <AppShell.Main>{children}</AppShell.Main>
+    </AppShell>
+  );
+}
---
+++ frontend/src/app/(site)/todos/page.tsx
@@ -0,0 +1,8 @@
+export default function TodosPage() {
+  return (
+    <div>
+      <h1>Todos</h1>
+      <p>This is the Todos page.</p>
+    </div>
+  );
+}
